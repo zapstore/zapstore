ecb0998b6a516f99a26d535062a965a03dbb8be2
franzap <_@franzap.com>
2024-06-17 17:44:22 -0300

    Local-first loading in updates screen

--- lib/screens/updates_screen.dart
+++ lib/screens/updates_screen.dart
@@ -18,12 +18,6 @@ class UpdatesScreen extends HookConsumerWidget {
         () => ref.apps.findAll(remote: true, params: {'installed': true})));
     final state = ref.apps.watchAll();
 
-    if (snapshot.connectionState == ConnectionState.waiting) {
-      return Center(
-        child: CircularProgressIndicator(),
-      );
-    }
-
     final updatableApps = state.model
         .where((app) => app.canUpdate)
         .toList()
@@ -33,64 +27,55 @@ class UpdatesScreen extends HookConsumerWidget {
         .toList()
         .sorted((a, b) => (a.name ?? '').compareTo(b.name ?? ''));
 
-    return RefreshIndicator(
-      onRefresh: () => ref.apps.findAll(params: {'installed': true}),
-      child: SingleChildScrollView(
-        physics: AlwaysScrollableScrollPhysics(),
-        child: Column(
-          key: UniqueKey(),
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return SingleChildScrollView(
+      physics: AlwaysScrollableScrollPhysics(),
+      child: Column(
+        key: UniqueKey(),
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text('Installed apps',
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+              Gap(20),
+              if (snapshot.connectionState == ConnectionState.waiting)
+                SizedBox(
+                    width: 16, height: 16, child: CircularProgressIndicator()),
+            ],
+          ),
+          if (updatableApps.isNotEmpty)
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text('Installed apps',
-                    style:
-                        TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
-                // Expanded(
-                //   child: TextButton(
-                //     style: TextButton.styleFrom(
-                //       foregroundColor: Colors.lightBlue,
-                //     ),
-                //     onPressed: () {},
-                //     child: const Text('Update All'),
-                //   ),
-                // ),
+                Gap(20),
+                Text(
+                  '${updatableApps.length} update${updatableApps.length > 1 ? 's' : ''} available'
+                      .toUpperCase(),
+                  style: TextStyle(
+                    fontSize: 16,
+                    letterSpacing: 3,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+                //
+                Gap(10),
+                for (final app in updatableApps) UpdatesAppCard(app: app),
               ],
             ),
-            if (updatableApps.isNotEmpty)
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Gap(20),
-                  Text(
-                    '${updatableApps.length} update${updatableApps.length > 1 ? 's' : ''} available'
-                        .toUpperCase(),
-                    style: TextStyle(
-                      fontSize: 16,
-                      letterSpacing: 3,
-                      fontWeight: FontWeight.w300,
-                    ),
-                  ),
-                  //
-                  Gap(10),
-                  for (final app in updatableApps) UpdatesAppCard(app: app),
-                ],
+          Gap(20),
+          if (updatedApps.isNotEmpty)
+            Text(
+              'Up to date'.toUpperCase(),
+              style: TextStyle(
+                fontSize: 16,
+                letterSpacing: 3,
+                fontWeight: FontWeight.w300,
               ),
-            Gap(20),
-            if (updatedApps.isNotEmpty)
-              Text(
-                'Up to date'.toUpperCase(),
-                style: TextStyle(
-                  fontSize: 16,
-                  letterSpacing: 3,
-                  fontWeight: FontWeight.w300,
-                ),
-              ),
-            Gap(10),
-            for (final app in updatedApps) UpdatesAppCard(app: app),
-          ],
-        ),
+            ),
+          Gap(10),
+          for (final app in updatedApps) UpdatesAppCard(app: app),
+        ],
       ),
     );
   }
