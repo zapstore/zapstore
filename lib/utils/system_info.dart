3e5f189ce3ea8a4271cf22046c747d0a105f9af9
franzap <_@franzap.com>
2024-06-17 22:00:56 -0300

    Add DB to system info

--- lib/utils/system_info.dart
+++ lib/utils/system_info.dart
@@ -1,8 +1,10 @@
 import 'package:android_package_manager/android_package_manager.dart';
 import 'package:device_info_plus/device_info_plus.dart';
+import 'package:flutter_data/flutter_data.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:system_info2/system_info2.dart';
 import 'package:zapstore/main.dart';
+import 'package:zapstore/main.data.dart';
 
 final deviceInfoPlugin = DeviceInfoPlugin();
 final packageManager = AndroidPackageManager();
@@ -10,17 +12,25 @@ final packageManager = AndroidPackageManager();
 final systemInfoProvider = FutureProvider((ref) async {
   final zsInfo =
       await packageManager.getPackageInfo(packageName: 'store.zap.app');
+  final dbInfo = {
+    'apps': ref.apps.countLocal,
+    'releases': ref.releases.countLocal,
+    'metadata': ref.fileMetadata.countLocal,
+  };
   return SystemInfo(
     androidInfo: await deviceInfoPlugin.androidInfo,
     zsInfo: zsInfo!,
+    dbInfo: dbInfo,
   );
 });
 
 class SystemInfo {
   final AndroidDeviceInfo androidInfo;
   final PackageInfo zsInfo;
+  final Map<String, int> dbInfo;
 
-  SystemInfo({required this.androidInfo, required this.zsInfo});
+  SystemInfo(
+      {required this.androidInfo, required this.zsInfo, required this.dbInfo});
 
   @override
   String toString() {
@@ -34,6 +44,8 @@ Display: ${androidInfo.display}
 Model: ${androidInfo.manufacturer} ${androidInfo.model} ${androidInfo.product}
 Host: ${androidInfo.host}
 Supported ABIs: ${androidInfo.supported64BitAbis}
+
+DB: $dbInfo
 
 Free/total memory: ${SysInfo.getFreePhysicalMemory() ~/ (1024 * 1024)}/${SysInfo.getTotalPhysicalMemory() ~/ (1024 * 1024)} MB
 Free/total virtual: ${SysInfo.getFreeVirtualMemory() ~/ (1024 * 1024)}/${SysInfo.getTotalVirtualMemory() ~/ (1024 * 1024)} MB
