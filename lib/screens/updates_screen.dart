7548de72a8995720f5017fa77308c4a7309f45ba
franzap <_@franzap.com>
2024-10-16 22:44:06 -0300

    Use pull to refresh in Updates screen

--- lib/screens/updates_screen.dart
+++ lib/screens/updates_screen.dart
@@ -27,56 +27,64 @@ class UpdatesScreen extends HookConsumerWidget {
         .toList()
         .sorted((a, b) => (a.name ?? '').compareTo(b.name ?? ''));
 
-    return SingleChildScrollView(
-      physics: AlwaysScrollableScrollPhysics(),
-      child: Column(
-        key: UniqueKey(),
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Text('Installed apps',
-                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
-              Gap(20),
-              if (snapshot.connectionState == ConnectionState.waiting)
-                SizedBox(
-                    width: 16, height: 16, child: CircularProgressIndicator()),
-            ],
-          ),
-          if (updatableApps.isNotEmpty)
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+    return RefreshIndicator(
+      onRefresh: () {
+        return ref.apps.findAll(remote: true, params: {'installed': true});
+      },
+      child: SingleChildScrollView(
+        physics: AlwaysScrollableScrollPhysics(),
+        child: Column(
+          key: UniqueKey(),
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
               children: [
+                Text('Installed apps',
+                    style:
+                        TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
                 Gap(20),
-                Text(
-                  '${updatableApps.length} update${updatableApps.length > 1 ? 's' : ''} available'
-                      .toUpperCase(),
-                  style: TextStyle(
-                    fontSize: 16,
-                    letterSpacing: 3,
-                    fontWeight: FontWeight.w300,
-                  ),
-                ),
-                //
-                Gap(10),
-                for (final app in updatableApps)
-                  AppCard(app: app, showUpdate: true),
+                if (snapshot.connectionState == ConnectionState.waiting)
+                  SizedBox(
+                      width: 16,
+                      height: 16,
+                      child: CircularProgressIndicator()),
               ],
             ),
-          Gap(20),
-          if (updatedApps.isNotEmpty)
-            Text(
-              'Up to date'.toUpperCase(),
-              style: TextStyle(
-                fontSize: 16,
-                letterSpacing: 3,
-                fontWeight: FontWeight.w300,
+            if (updatableApps.isNotEmpty)
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Gap(20),
+                  Text(
+                    '${updatableApps.length} update${updatableApps.length > 1 ? 's' : ''} available'
+                        .toUpperCase(),
+                    style: TextStyle(
+                      fontSize: 16,
+                      letterSpacing: 3,
+                      fontWeight: FontWeight.w300,
+                    ),
+                  ),
+                  //
+                  Gap(10),
+                  for (final app in updatableApps)
+                    AppCard(app: app, showUpdate: true),
+                ],
               ),
-            ),
-          Gap(10),
-          for (final app in updatedApps) AppCard(app: app),
-        ],
+            Gap(20),
+            if (updatedApps.isNotEmpty)
+              Text(
+                'Up to date'.toUpperCase(),
+                style: TextStyle(
+                  fontSize: 16,
+                  letterSpacing: 3,
+                  fontWeight: FontWeight.w300,
+                ),
+              ),
+            Gap(10),
+            for (final app in updatedApps) AppCard(app: app),
+          ],
+        ),
       ),
     );
   }
