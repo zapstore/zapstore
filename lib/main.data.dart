1c2831a1b274aff9bd835fafd684dba0a883c209
franzap <_@franzap.com>
2024-11-08 20:51:57 -0300

    Update gradle, remove warnings

--- lib/main.data.dart
+++ lib/main.data.dart
@@ -1,5 +1,3 @@
-
-
 // GENERATED CODE - DO NOT MODIFY BY HAND
 // ignore_for_file: directives_ordering, top_level_function_literal_block, depend_on_referenced_packages
 
@@ -16,31 +14,40 @@ import 'package:zapstore/models/user.dart';
 
 final adapterProvidersMap = <String, Provider<Adapter<DataModelMixin>>>{
   'appCurationSets': appCurationSetsAdapterProvider,
-'apps': appsAdapterProvider,
-'fileMetadata': fileMetadataAdapterProvider,
-'localApps': localAppsAdapterProvider,
-'releases': releasesAdapterProvider,
-'settings': settingsAdapterProvider,
-'users': usersAdapterProvider
+  'apps': appsAdapterProvider,
+  'fileMetadata': fileMetadataAdapterProvider,
+  'localApps': localAppsAdapterProvider,
+  'releases': releasesAdapterProvider,
+  'settings': settingsAdapterProvider,
+  'users': usersAdapterProvider
 };
 
 extension AdapterWidgetRefX on WidgetRef {
-  Adapter<AppCurationSet> get appCurationSets => watch(appCurationSetsAdapterProvider)..internalWatch = watch;
+  Adapter<AppCurationSet> get appCurationSets =>
+      watch(appCurationSetsAdapterProvider)..internalWatch = watch;
   Adapter<App> get apps => watch(appsAdapterProvider)..internalWatch = watch;
-  Adapter<FileMetadata> get fileMetadata => watch(fileMetadataAdapterProvider)..internalWatch = watch;
-  Adapter<LocalApp> get localApps => watch(localAppsAdapterProvider)..internalWatch = watch;
-  Adapter<Release> get releases => watch(releasesAdapterProvider)..internalWatch = watch;
-  Adapter<Settings> get settings => watch(settingsAdapterProvider)..internalWatch = watch;
+  Adapter<FileMetadata> get fileMetadata =>
+      watch(fileMetadataAdapterProvider)..internalWatch = watch;
+  Adapter<LocalApp> get localApps =>
+      watch(localAppsAdapterProvider)..internalWatch = watch;
+  Adapter<Release> get releases =>
+      watch(releasesAdapterProvider)..internalWatch = watch;
+  Adapter<Settings> get settings =>
+      watch(settingsAdapterProvider)..internalWatch = watch;
   Adapter<User> get users => watch(usersAdapterProvider)..internalWatch = watch;
 }
 
 extension AdapterRefX on Ref {
-
-  Adapter<AppCurationSet> get appCurationSets => watch(appCurationSetsAdapterProvider)..internalWatch = watch as Watcher;
-  Adapter<App> get apps => watch(appsAdapterProvider)..internalWatch = watch as Watcher;
-  Adapter<FileMetadata> get fileMetadata => watch(fileMetadataAdapterProvider)..internalWatch = watch as Watcher;
-  Adapter<LocalApp> get localApps => watch(localAppsAdapterProvider)..internalWatch = watch as Watcher;
-  Adapter<Release> get releases => watch(releasesAdapterProvider)..internalWatch = watch as Watcher;
-  Adapter<Settings> get settings => watch(settingsAdapterProvider)..internalWatch = watch as Watcher;
-  Adapter<User> get users => watch(usersAdapterProvider)..internalWatch = watch as Watcher;
+  Adapter<AppCurationSet> get appCurationSets =>
+      watch(appCurationSetsAdapterProvider)..internalWatch = watch;
+  Adapter<App> get apps => watch(appsAdapterProvider)..internalWatch = watch;
+  Adapter<FileMetadata> get fileMetadata =>
+      watch(fileMetadataAdapterProvider)..internalWatch = watch;
+  Adapter<LocalApp> get localApps =>
+      watch(localAppsAdapterProvider)..internalWatch = watch;
+  Adapter<Release> get releases =>
+      watch(releasesAdapterProvider)..internalWatch = watch;
+  Adapter<Settings> get settings =>
+      watch(settingsAdapterProvider)..internalWatch = watch;
+  Adapter<User> get users => watch(usersAdapterProvider)..internalWatch = watch;
 }
