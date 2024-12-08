7c4f0db377b2cfaed66a97f605db5862cd59e1a8
franzap <_@franzap.com>
2024-12-07 21:29:21 -0300

    Full screen images, closes #181

--- lib/screens/app_detail_screen.dart
+++ lib/screens/app_detail_screen.dart
@@ -1,5 +1,6 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:easy_image_viewer/easy_image_viewer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
@@ -94,12 +95,24 @@ class AppDetailScreen extends HookConsumerWidget {
                               child: Row(
                                 children: [
                                   for (final i in app.images)
-                                    Padding(
-                                      padding: const EdgeInsets.only(right: 12),
-                                      child: CachedNetworkImage(
-                                        imageUrl: i,
-                                        errorWidget: (_, __, ___) =>
-                                            Container(),
+                                    GestureDetector(
+                                      onTap: () {
+                                        final imageProvider =
+                                            CachedNetworkImageProvider(i);
+                                        showImageViewer(
+                                          context,
+                                          imageProvider,
+                                          doubleTapZoomable: true,
+                                        );
+                                      },
+                                      child: Padding(
+                                        padding:
+                                            const EdgeInsets.only(right: 12),
+                                        child: CachedNetworkImage(
+                                          imageUrl: i,
+                                          errorWidget: (_, __, ___) =>
+                                              Container(),
+                                        ),
                                       ),
                                     ),
                                 ],
